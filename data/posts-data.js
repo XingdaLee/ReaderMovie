90def05f076dffdf468ff7806c2ff8db0140c00f
Sinder <1028263368@qq.com>
2017-07-03 22:57:25 +0800

    业务数据和代码的分离require

---
+++ data/posts-data.js
@@ -0,0 +1,55 @@
+var post_data = [
+  {
+    isShowAuthor: true,
+    date: "2016年7月",
+    title: "阳澄湖大闸蟹上市啦",
+    imgSrc: "../../images/post/xie.jpg",
+    avatar: "/images/avatar/girl.png",
+    content: "中华绒螯蟹是一种经济蟹类，又称河蟹、毛蟹、清水蟹、大闸蟹。为中国久负盛名的美食。其螯足用于取食和抗敌，掌部内外缘密生绒毛，绒螯蟹因此而得名。杂食性动物，鱼、虾、螺、蚌、蠕虫、蚯蚓、昆虫及其幼虫等均可作为大闸蟹的动物性饵料。",
+    reading: "1000+",
+    collection: "5236"
+  },
+  {
+    isShowAuthor: true,
+    date: "2016年7月",
+    title: "奔驰汽车销售指南",
+    imgSrc: "../../images/post/car.jpg",
+    avatar: "/images/avatar/girl.png",
+    content: "奔驰，德国汽车品牌，被认为是世界上最成功的高档汽车品牌之一，其完美的技术水平、过硬的质量标准、推陈出新的创新能力、以及一系列经典轿跑车款式令人称道。奔驰三叉星已成为世界上最著名的汽车及品牌标志之一。",
+    reading: "1000+",
+    collection: "5236"
+  },
+  {
+    isShowAuthor: true,
+    date: "2016年8月",
+    title: "比亚迪唐评测",
+    imgSrc: "../../images/post/byd.jpg",
+    avatar: "/images/avatar/girl.png",
+    content: "2017深港澳车展上，比亚迪2017款唐正式上市，新车分为全时四驱豪华型、全时四驱豪雅型两款车型，售价区间为26.59万元-27.99万元。另外，补贴方面，在享受3.6万元政策支持（含国补、地补、比亚迪电动未来基金）以及最高可节省约2.5万元购置税、车船税等后，比亚迪还将额外给予首批1万名用户各1万元的电费。",
+    reading: "1000+",
+    collection: "5236"
+  },
+  {
+    isShowAuthor: true,
+    date: "2016年7月",
+    title: "啦啦啦啦啦",
+    imgSrc: "../../images/post/car.jpg",
+    avatar: "/images/avatar/girl.png",
+    content: "奔驰，德国汽车品牌，被认为是世界上最成功的高档汽车品牌之一，其完美的技术水平、过硬的质量标准、推陈出新的创新能力、以及一系列经典轿跑车款式令人称道。奔驰三叉星已成为世界上最著名的汽车及品牌标志之一。",
+    reading: "1000+",
+    collection: "5236"
+  },
+  {
+    isShowAuthor: true,
+    date: "2016年7月",
+    title: "呵呵呵呵呵",
+    imgSrc: "../../images/post/car.jpg",
+    avatar: "/images/avatar/girl.png",
+    content: "奔驰，德国汽车品牌，被认为是世界上最成功的高档汽车品牌之一，其完美的技术水平、过硬的质量标准、推陈出新的创新能力、以及一系列经典轿跑车款式令人称道。奔驰三叉星已成为世界上最著名的汽车及品牌标志之一。",
+    reading: "1000+",
+    collection: "5236"
+  }
+];
+module.exports = {
+  post_contentList: post_data
+};
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,7 +1,9 @@
+var postsData = require('../../data/posts-data.js');
 Page({
 
   /**
    * 页面的初始数据
+   * data 的执行是在onLoad后执行的
    */
   data: {
     post_contentList: []
@@ -13,30 +15,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var post_content = [
-      {
-        isShowAuthor: true,
-        date: "2016年7月",
-        title: "阳澄湖大闸蟹上市啦",
-        post_image: "../../images/post/xie.jpg",
-        author_image: "/images/avatar/girl.png",
-        content: "中华绒螯蟹是一种经济蟹类，又称河蟹、毛蟹、清水蟹、大闸蟹。为中国久负盛名的美食。其螯足用于取食和抗敌，掌部内外缘密生绒毛，绒螯蟹因此而得名。杂食性动物，鱼、虾、螺、蚌、蠕虫、蚯蚓、昆虫及其幼虫等均可作为大闸蟹的动物性饵料。",
-        collect_num: "1000+",
-        view_num: "5236"
-      },
-      {
-        isShowAuthor: true,
-        date: "2016年7月",
-        title: "奔驰汽车销售指南",
-        post_image: "../../images/post/car.jpg",
-        author_image: "/images/avatar/girl.png",
-        content: "奔驰，德国汽车品牌，被认为是世界上最成功的高档汽车品牌之一，其完美的技术水平、过硬的质量标准、推陈出新的创新能力、以及一系列经典轿跑车款式令人称道。奔驰三叉星已成为世界上最著名的汽车及品牌标志之一。",
-        collect_num: "1000+",
-        view_num: "5236"
-      }
-    ];
     this.setData({
-      post_contentList: post_content
+      post_contentList: postsData.post_contentList
     });
     this.process('test process Func');
     console.log("onLoad")
